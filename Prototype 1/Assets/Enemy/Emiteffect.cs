3941c3d048267509003135b1cf8030f3605c074d
Unknown <pedicho@gmail.com>
2019-02-19 14:07:02 -0700

    daily commit

---
+++ Prototype 1/Assets/Enemy/Emiteffect.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Emiteffect : MonoBehaviour {
+
+	public GameObject FireEffect;
+	void Update ()
+	{
+		 Instantiate(FireEffect, transform.position, transform.rotation);	
+	}
+}
