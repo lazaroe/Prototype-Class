073aac3567f022468102a0cff089051dd40b5d36
Unknown <pedicho@gmail.com>
2019-02-26 12:31:42 -0700

    daily commit

---
+++ Prototype 1/Assets/HighilightItem.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HighilightItem : MonoBehaviour {
+
+	public bool InRange;
+	public GameObject Highlighter;
+
+private void OnTriggerEnter(Collider obj)
+{
+	InRange = true;
+	Highlighter.SetActive(true);
+}
+private void OnTriggerExit(Collider obj)
+{
+	if (obj.CompareTag("Player"))
+	{
+		InRange = false;
+		Highlighter.SetActive(false);
+	}
+}
+}
