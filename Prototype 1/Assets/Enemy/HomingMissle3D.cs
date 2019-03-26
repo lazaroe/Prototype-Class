79d99ea357bbc1670b6fbb027c062dc5e16694ff
Unknown <pedicho@gmail.com>
2019-03-26 11:54:26 -0600

    daily commit

---
+++ Prototype 1/Assets/Enemy/HomingMissle3D.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HomingMissle3D : MonoBehaviour {
+
+	public Transform rocketTarget;
+	public Rigidbody rocketRigidbody;
+	public float turnR;
+	public float rocketVelocity;
+
+	private void FixedUpdate()
+	{
+		rocketRigidbody.velocity = transform.forward * rocketVelocity;
+		var rocketTargetRotation = Quaternion.LookRotation(rocketTarget.position - transform.position);
+		rocketRigidbody.MoveRotation (Quaternion.RotateTowards(transform.rotation, rocketTargetRotation, turnR));
+	}
+}
